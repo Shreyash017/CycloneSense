6b29299c714470d9188f36fefc305f44eb65f224
Shreyash Salunke <shreyashsalunkesds@gmail.com>
2023-06-29 15:04:26 +0530

    Home Page Added

--- Website/main/urls.py
+++ Website/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('analysis/', views.analysis, name='analysis'),
-    path('about/', views.about, name='about'),
+    path('analysis', views.analysis, name='analysis'),
+    path('about', views.about, name='about'),
 ]
